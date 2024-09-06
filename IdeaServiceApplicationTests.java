70faa012b088648b0e557d63f1ebb61034048748
OM SONI <166503419+mrOmsoni@users.noreply.github.com>
2024-09-07 00:12:43 +0530

    Add files via upload
    Built an Application using SpringBoot

---
+++ IdeaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SERVICE.IDEA.IDEA_SERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
